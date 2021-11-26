63fe2fb1a211780aaf3a79f621aba817aac03d86
littlecontrol <i@littlecontrol.me>
2021-11-26 17:16:16 +0800

    tour: if关键字基本使用

--- tours/flow_control.go
+++ tours/flow_control.go
@@ -2,28 +2,23 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
 
+func mySqrt(x float64) string {
+	if (x < 0) {
+		return mySqrt(-x) + "i"
+	}
+	return fmt.Sprint(math.Sqrt(x))
+}
+
 func main() {
-	sum := 1
-	for i := 1; i < 5; i++ {
-		sum += i
-	}
-	fmt.Println(sum)
-	for ; sum > 2; {
-		sum -= 1
-	}
-	fmt.Println(sum)
-	for sum < 5 {
-		sum += 1
-	}
-	fmt.Println("The Wa Lu Du")
-	fmt.Println(sum)
+	fmt.Println(mySqrt(2), mySqrt(-4))
 }
 
 /*
 	在go中,只有一个循环关键字 -> for
-	for 循环的三个组件那里没有括号, 循环体大括号不可省略
+	for 循环的三个组件那里可以没有括号, 循环体大括号不可省略
 	循环的第一个和第三个组件可以省略, 分号也可以省略
 	循环的第二个组件也可省略,省略后将无限循环
 */
