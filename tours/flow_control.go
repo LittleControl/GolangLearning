1201e21847b7af0f65322db8a1d535384503629f
littlecontrol <i@littlecontrol.me>
2021-11-29 11:13:29 +0800

    tour: 循环流程练习

--- tours/flow_control.go
+++ tours/flow_control.go
@@ -5,20 +5,19 @@ import (
 	"math"
 )
 
-func mySqrt(x float64) string {
-	if (x < 0) {
-		return mySqrt(-x) + "i"
+// z -= (z*z - x) / (2*z)
+func mySqrt(x float64) float64 {
+	var res float64 = 1
+	for count := 0; count < 10; count++ {
+		res -= (res * res - x) / (2 * res)
+		fmt.Println(res)
 	}
-	return fmt.Sprint(math.Sqrt(x))
+	return res
 }
 
 func main() {
-	// fmt.Println(mySqrt(2), mySqrt(-4))
-	if num0 := 9; num0 < 0 {
-		fmt.Println(num0)
-	} else {
-		fmt.Println("else is here")
-	}
+	fmt.Println(mySqrt(2))
+	fmt.Println(math.Sqrt(2))
 }
 
 /*
@@ -26,4 +25,8 @@ func main() {
 	for 循环的三个组件那里可以没有括号, 循环体大括号不可省略
 	循环的第一个和第三个组件可以省略, 分号也可以省略
 	循环的第二个组件也可省略,省略后将无限循环
+*/
+
+/*
+if 和 else中条件中定义的变量,只能在自己的块范围内引用
 */
